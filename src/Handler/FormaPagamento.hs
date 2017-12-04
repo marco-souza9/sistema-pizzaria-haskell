46441fd1123fce1860dbe7a6b9bde71376c32a48
romefeller <pedro.spaolonzi@gmail.com>
2017-12-04 22:43:30 +0000

    Pagina de gerenciamento da forma de pagamento

--- src/Handler/FormaPagamento.hs
+++ src/Handler/FormaPagamento.hs
@@ -1,20 +1,40 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeFamilies, DeriveGeneric #-}
 module Handler.FormaPagamento where
 
 import Import
 
-getAdmFormPagCadastrarR :: Handler Html
-getAdmFormPagCadastrarR = undefined
-
 postAdmFormPagCadastrarR :: Handler Html
 postAdmFormPagCadastrarR = undefined
 
-getAdmFormPagDetalharR :: FormaPagamentoId -> Handler Html
-getAdmFormPagDetalharR id = undefined
-
 postAdmFormPagAlterarR :: Handler Html
 postAdmFormPagAlterarR = undefined
+
+getAdmFormPagGerenciarR :: Handler Html
+getAdmFormPagGerenciarR = do
+    defaultLayout $ do
+        setTitle "Gerenciar Forma de Pagamento"
+        --css estático
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addStylesheet $ (StaticR css_font_awesome_min_css)
+        addStylesheet $ (StaticR css_jquery_ui_css)
+        addStylesheet $ (StaticR css_main_css)
+        addStylesheet $ (StaticR css_normalize_css)
+        addStylesheet $ (StaticR css_picto_foundry_food_css)
+        addStylesheet $ (StaticR css_style_portfolio_css)
+        --corpo html
+        $(whamletFile "templates/commons/navbaradm.hamlet")
+        $(whamletFile "templates/gerenciarFormaPagamento.hamlet")
+        --javascript estátic7o
+        addScript $ (StaticR js_jquery_1_10_2_min_js) 
+        addScript $ (StaticR js_jquery_1_10_2_js)        
+        addScript $ (StaticR js_jquery_mixitup_min_js)
+        addScript $ (StaticR js_bootstrap_min_js)
+        addScript $ (StaticR js_jquery_mask_js)
+        addScript $ (StaticR js_jquery_mask_min_js)
+        addScript $ (StaticR js_mascaras_js)
+        addScript $ (StaticR js_main_js) 
