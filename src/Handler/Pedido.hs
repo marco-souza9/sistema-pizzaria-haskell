66150f9e00ec174f1acbd00a84d279f58d8f3cbb
romefeller <mass_2008@hotmail.com>
2017-12-05 00:11:10 +0000

    Listagem dos pedidos abertos para o adm do sistema.

--- src/Handler/Pedido.hs
+++ src/Handler/Pedido.hs
@@ -34,6 +34,8 @@ buscarPedidos (Just valor) = runDB $ selectList [PedidoUsuario ==. (chave)] []
     where
         valorInt = read (unpack valor) :: Int
         chave = toSqlKey (fromIntegral (valorInt) ) :: UsuarioId
+        
+
 
 getPedidoListarR :: Handler Html
 getPedidoListarR = do
@@ -62,10 +64,35 @@ getPedidoListarR = do
         addScript $ (StaticR js_mascaras_js)
         addScript $ (StaticR js_main_js) 
 
-getPedidoDetalharR :: Handler Html
-getPedidoDetalharR = undefined
 getPedidoAberDetalharR :: Handler Html
-getPedidoAberDetalharR = undefined
+getPedidoAberDetalharR = do
+    usuId <- lookupSession "usuId"
+    pedidos <- buscarPedidos $ usuId
+    defaultLayout $ do
+        setTitle "Carrinho"
+        --css estático
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addStylesheet $ (StaticR css_font_awesome_min_css)
+        addStylesheet $ (StaticR css_jquery_ui_css)
+        addStylesheet $ (StaticR css_main_css)
+        addStylesheet $ (StaticR css_normalize_css)
+        addStylesheet $ (StaticR css_picto_foundry_food_css)
+        addStylesheet $ (StaticR css_style_portfolio_css)
+        --corpo html
+        $(whamletFile "templates/commons/navbarcli.hamlet")
+        $(whamletFile "templates/carrinho.hamlet")
+        --javascript estátic7o
+        addScript $ (StaticR js_jquery_1_10_2_min_js) 
+        addScript $ (StaticR js_jquery_1_10_2_js)        
+        addScript $ (StaticR js_jquery_mixitup_min_js)
+        addScript $ (StaticR js_bootstrap_min_js)
+        addScript $ (StaticR js_jquery_mask_js)
+        addScript $ (StaticR js_jquery_mask_min_js)
+        addScript $ (StaticR js_mascaras_js)
+        addScript $ (StaticR js_main_js) 
+
+postPedidoAberAdicionarR :: PedidoId -> Handler Html
+postPedidoAberAdicionarR pid = undefined
 
 postPedidoAberAlterarR :: Handler Html
 postPedidoAberAlterarR = undefined
@@ -76,8 +103,60 @@ getPedidoAberConcluirR = undefined
 postPedidoAberConcluirR :: Handler Html
 postPedidoAberConcluirR = undefined
 
+
+mostraPedido :: Entity Pedido -> Widget
+mostraPedido (Entity pid pedido) =  do
+    usuario <- handlerToWidget $ runDB $ get404 $ pedidoUsuario pedido
+    [whamlet|
+            <table>
+                <tr>
+                    <td width="30%">
+                        <p>
+                            <b>#{usuarioNome usuario}
+                        <p style="margin-top:3px;">#{usuarioCpf usuario}
+                        <p style="margin-top:3px;">#{formatarData $ pedidoData pedido}
+                     
+                    <td>
+                        <select>
+                            <option value="pizza">Em aberto
+                            <option value="bebida">Montando
+                            <option value="sobremesa">Finalizado
+                        
+                    <td width="15%">
+                    <td width="20%">
+                        <p>
+                            <h2>R$#{show $ pedidoTotal pedido} 
+            
+                    <td width="10%">
+                        <button class="btn btn-success" style="font-size:16px">Detalhar
+            <hr>
+    |] 
+
 getAdmPedidoListarR :: Handler Html
-getAdmPedidoListarR = undefined
+getAdmPedidoListarR = do
+    pedidos <- runDB $ selectList [PedidoStatus !=. 4, PedidoStatus !=. 5] []
+    defaultLayout $ do
+        setTitle "Pedidos listados"
+        --css estático
+        addStylesheet $ (StaticR css_bootstrap_css)
+        addStylesheet $ (StaticR css_font_awesome_min_css)
+        addStylesheet $ (StaticR css_jquery_ui_css)
+        addStylesheet $ (StaticR css_main_css)
+        addStylesheet $ (StaticR css_normalize_css)
+        addStylesheet $ (StaticR css_picto_foundry_food_css)
+        addStylesheet $ (StaticR css_style_portfolio_css)
+        --corpo html
+        $(whamletFile "templates/commons/navbaradm.hamlet")
+        $(whamletFile "templates/listarPedidosAdm.hamlet")
+        --javascript estátic7o
+        addScript $ (StaticR js_jquery_1_10_2_min_js) 
+        addScript $ (StaticR js_jquery_1_10_2_js)        
+        addScript $ (StaticR js_jquery_mixitup_min_js)
+        addScript $ (StaticR js_bootstrap_min_js)
+        addScript $ (StaticR js_jquery_mask_js)
+        addScript $ (StaticR js_jquery_mask_min_js)
+        addScript $ (StaticR js_mascaras_js)
+        addScript $ (StaticR js_main_js) 
 
 getAdmPedidoDetalharR :: PedidoId -> Handler Html
 getAdmPedidoDetalharR id = undefined
